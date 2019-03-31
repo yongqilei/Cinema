c25eeccb89e65751a6c473c1cf8cd556cce8241f
rayyq <2540356035@qq.com>
2019-03-31 14:58:46 +0900

    修改LoginController错误代码

--- user-service/src/main/java/com/movie/pncinema/us/controller/LoginController.java
+++ user-service/src/main/java/com/movie/pncinema/us/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.movie.pncinema.common.dto.JsonResponse;
+import com.movie.pncinema.common.dto.ResponseEntity;
 import com.movie.pncinema.user.api.entity.po.AccountPO;
 import com.movie.pncinema.user.api.service.LoginService;
 
