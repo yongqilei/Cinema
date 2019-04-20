05caf8ec844fdeb11ebaac312269fd0377e4a260
leiyongqi <2540356035@qq.com>
2019-04-20 22:20:30 +0900

    添加Swagger注解

--- user-service/src/main/java/com/movie/pncinema/us/controller/LoginController.java
+++ user-service/src/main/java/com/movie/pncinema/us/controller/LoginController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import com.movie.pncinema.utils.captcha.CaptchaUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +14,14 @@ import com.movie.pncinema.user.api.entity.po.AccountPO;
 import com.movie.pncinema.user.api.service.LoginService;
 
 @RestController
+@Api(value = "用户登录接口")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
     @GetMapping("/user/api/list")
+    @ApiOperation(value = "用户查询接口（测试）", notes = "用于测试数据库链接是否正常")
     public ResponseEntity list() {
         ResponseEntity result = new ResponseEntity();
         List<AccountPO> list = loginService.list();
--- user-service/src/main/java/com/movie/pncinema/us/controller/RegisterController.java
+++ user-service/src/main/java/com/movie/pncinema/us/controller/RegisterController.java
@@ -3,6 +3,9 @@ package com.movie.pncinema.us.controller;
 import com.movie.pncinema.common.dto.ResponseEntity;
 import com.movie.pncinema.us.dao.AccountMapper;
 import com.movie.pncinema.utils.captcha.CaptchaUtil;
+import com.movie.pncinema.utils.redis.RedisUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
+@Api(value = "用户注册接口")
 public class RegisterController {
 
     @Autowired
     private AccountMapper accountMapper;
 
-    @PostMapping("/catpcha")
+    @PostMapping("/user/catpcha")
+    @ApiOperation(value = "获取验证码", notes = "根据输入的用户名来声称验证码，未输入需要注册的用户名则无法生成验证码。")
     public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
         try {
             CaptchaUtil.out(70, 30, 4, request, response);
@@ -31,10 +36,17 @@ public class RegisterController {
      * @param username 注册时填写的用户名
      * @return 是否重复
      */
-    @PostMapping("/")
+    @PostMapping("/user/testredis")
+    @ApiOperation(value = "验证用户名是否存在（是否已经被注册）")
     public ResponseEntity validUsername(String username) {
         // TODO: 验证用户名是否重复，注册用
-        return null;
+        RedisUtils.set("test", "Hello World", 120L);
+        ResponseEntity result = new ResponseEntity();
+        result.setCode(200);
+        result.setData(RedisUtils.exists("test"));
+        result.setMessage("测试redis");
+
+        return result;
     }
 
 
