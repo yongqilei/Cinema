6a516d4a8110720a225edded04adc03754ca8576
songyingzhe <478674425@qq.com>
2019-04-22 19:48:48 +0900

    日志打印

--- commons-utils/src/main/java/com/movie/pncinema/utils/rsa/RSATools.java
+++ commons-utils/src/main/java/com/movie/pncinema/utils/rsa/RSATools.java
@@ -34,11 +34,13 @@ import org.apache.log4j.Logger;
  */
 public class RSATools {
 
+	public static final int RSA_KEY_SiZE = 1024;
+
 	private static final String DEFAULT_PRIVATE_KEY_STRING = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAocbCrurZGbC5GArEHKlAfDSZi7gFBnd4yxOt0rwTqKBFzGyhtQLu5PRKjEiOXVa95aeIIBJ6OhC2f8FjqFUpawIDAQABAkAPejKaBYHrwUqUEEOe8lpnB6lBAsQIUFnQI/vXU4MV+MhIzW0BLVZCiarIQqUXeOhThVWXKFt8GxCykrrUsQ6BAiEA4vMVxEHBovz1di3aozzFvSMdsjTcYRRo82hS5Ru2/OECIQC2fAPoXixVTVY7bNMeuxCP4954ZkXp7fEPDINCjcQDywIgcc8XLkkPcs3Jxk7uYofaXaPbg39wuJpEmzPIxi3k0OECIGubmdpOnin3HuCP/bbjbJLNNoUdGiEmFL5hDI4UdwAdAiEAtcAwbm08bKN7pwwvyqaCBC//VnEWaq39DCzxr+Z2EIk=";
 	public static final String DEFAULT_PUBLIC_KEY_STRING = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKHGwq7q2RmwuRgKxBypQHw0mYu4BQZ3eMsTrdK8E6igRcxsobUC7uT0SoxIjl1WveWniCASejoQtn/BY6hVKWsCAwEAAQ==";
 	
 	private static final Logger LOG = Logger.getLogger(RSATools.class);
-	
+
 	public static String decrypt(String cipherText) throws Exception {
 		return decrypt((String) null, cipherText);
 	}
